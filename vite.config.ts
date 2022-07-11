14a63c43af856bfa45042643e0dd66c9c895b253
Simon <47527944+Frooastside@users.noreply.github.com>
2022-07-11 18:09:58 +0200

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { dependencies } from "./package.json";
+
+function renderChunks(dependencies: Record<string, string>) {
+  const chunks: {
+    [key: string]: unknown;
+  } = {};
+  Object.keys(dependencies).forEach((key) => {
+    if (["react", "react-dom", "react-router-dom"].includes(key)) return;
+    chunks[key] = [key];
+  });
+  return chunks;
+}
+
+export default defineConfig({
+  server: {
+    port: 3000,
+    hmr: {
+      clientPort: 443
+    }
+  },
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom", "react-router-dom"],
+          ...renderChunks(dependencies)
+        }
+      }
+    }
+  }
+});
